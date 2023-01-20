739e3f231b29fcdacd42fba1a291eabdfa42aab4
Erdum <erdumadnan@gmail.com>
2023-01-20 21:44:52 +0500

    Update uart driver implemented

---
+++ flight_controller/include/uart.h
@@ -0,0 +1,2 @@
+void uart1_init(uint32_t baud_rate);
+void uart1_write(char *string);
---
+++ flight_controller/src/uart.c
@@ -0,0 +1,47 @@
+#include "stm32f1xx.h"
+
+static void uart1_send_byte(uint8_t byte)
+{
+    USART1->DR = byte;
+
+    // Wait for Transmit data register to be empty
+    while (!(USART1->SR & USART_SR_TXE));
+}
+
+void uart1_init(uint32_t baud_rate)
+{
+    // Enable APB2 Bus for GPIOA Module
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+
+    // Reset PA9
+    GPIOA->CRH &= ~(GPIO_CRH_MODE9);
+    GPIOA->CRH &= ~(GPIO_CRH_CNF9);
+
+    // Set PA9 to Output Mode max 10MHZ
+    GPIOA->CRH |= GPIO_CRH_MODE9_0;
+    GPIOA->CRH &= ~(GPIO_CRH_MODE9_1);
+
+    // SET PA9 Alternative function output Push Pull
+    GPIOA->CRH &= ~(GPIO_CRH_CNF9_0);
+    GPIOA->CRH |= GPIO_CRH_CNF9_1;
+
+    // Enable APB2 Bus for USART1 Module
+    RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+
+    // Enable USART module and TX
+    USART1->CR1 = USART_CR1_UE | USART_CR1_TE;
+
+    SystemCoreClockUpdate();
+
+    uint16_t division = SystemCoreClock / baud_rate;
+    USART1->BRR = division;
+}
+
+
+void uart1_write(char *string)
+{
+    while(*string) {
+        uart1_send_byte(*string);
+        string++;
+    }
+}
