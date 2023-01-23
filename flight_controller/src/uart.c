cd1e62eed56d9ba0ecb130dc4e478af5a482fc2c
Erdum <erdumadnan@gmail.com>
2023-01-23 10:01:16 +0500

    Update comment

--- flight_controller/src/uart.c
+++ flight_controller/src/uart.c
@@ -17,8 +17,8 @@ void init_usart1()
     GPIOA->CRH &= ~(GPIO_CRH_CNF9  | GPIO_CRH_MODE9);   // reset PA9
     GPIOA->CRH &= ~(GPIO_CRH_CNF10 | GPIO_CRH_MODE10);  // reset PA10
     
-    GPIOA->CRH |= GPIO_CRH_MODE9_0;  // 0b11 10MHz output
-    GPIOA->CRH &= ~GPIO_CRH_MODE9_1;  // 0b11 10MHz output
+    GPIOA->CRH |= GPIO_CRH_MODE9_0;  // 0b01 10MHz output
+    GPIOA->CRH &= ~GPIO_CRH_MODE9_1;  // 0b01 10MHz output
     GPIOA->CRH |= GPIO_CRH_CNF9_1;    // PA9: output @ 10MHz - Alt-function Push-pull
 
     GPIOA->CRH |= GPIO_CRH_CNF10_0;   // PA10 RX - Mode = 0b00 (input) - CNF = 0b01 (input floating)
