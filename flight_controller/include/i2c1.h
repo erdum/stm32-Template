ad3d395894378ba472a4ca4cc487c9ca1ae9186f
Erdum <erdumadnan@gmail.com>
2023-01-28 23:57:58 +0500

    Update methods available to linker

--- flight_controller/include/i2c1.h
+++ flight_controller/include/i2c1.h
@@ -1,5 +1,5 @@
-void init_i2c1_master(void);
-uint8_t i2c1_read_byte(uint8_t device_address);
-void i2c1_read_buffer(uint8_t device_address, uint8_t *buffer, uint8_t sizeof_buffer);
-void i2c1_write_byte(uint8_t device_address, uint8_t data, uint8_t stop);
-void i2c1_write_buffer(uint8_t device_address, uint8_t *buffer, uint8_t sizeof_buffer);
+extern void init_i2c1_master(void);
+extern uint8_t i2c1_read_byte(uint8_t device_address);
+extern void i2c1_read_buffer(uint8_t device_address, uint8_t *buffer, uint8_t sizeof_buffer);
+extern void i2c1_write_byte(uint8_t device_address, uint8_t data, uint8_t stop);
+extern void i2c1_write_buffer(uint8_t device_address, uint8_t *buffer, uint8_t sizeof_buffer);
