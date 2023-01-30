d06a7d7521aaa318d681e53457c060d083feefca
Erdum <erdumadnan@gmail.com>
2023-01-30 22:50:07 +0500

    Update testing communication drivers

--- flight_controller/src/main.c
+++ flight_controller/src/main.c
@@ -1,7 +1,11 @@
 #include <stdint.h>
+#include <stdio.h>
 #include "stm32f1xx.h"
-#include "uart.h"
-#include "i2c.h"
+#include "usart1.h"
+#include "i2c1.h"
+#include "spi1.h"
+
+#define DEVICE 0x68
 
 int main(void) 
 {
@@ -14,12 +18,20 @@ int main(void)
     usart1_write_string("STM32 Initialized\n");
     
     while (1) {
-        uint8_t str[5];
-        i2c1_read_buffer(0x08, str, sizeof(str));
+        uint8_t pwr_reg[2];
+        pwr_reg[0] = 107U;
+        pwr_reg[1] = 0U;
+        i2c1_write_buffer(DEVICE, pwr_reg, sizeof(pwr_reg));
+        for(int i = 0; i < 100000; i++);
 
-        for (uint8_t i = 0; i < (sizeof(str) / sizeof(uint8_t)); i++) {
-            usart1_write_byte(str[i]);
-        }
+        uint8_t data[4];
+        i2c1_write_byte(DEVICE, 63U, 0U);
+        i2c1_read_buffer(DEVICE, data, sizeof(data));
+
+        int16_t raw_acc_z = ((data[0] << 8U) | data[1]);
+        char str[10];
+        sprintf(str, "%u", raw_acc_z);
+        usart1_write_string(str);
         usart1_write_string("\n");
 
         for(int i = 0; i < 100000; i++);
