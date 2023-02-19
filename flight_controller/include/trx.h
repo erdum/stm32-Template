2870cb74928eb88ac457ab83ea2d7a28a0a5aaee
Erdum <erdumadnan@gmail.com>
2023-02-19 22:29:02 +0500

    Update testing nrf240l+

--- flight_controller/include/trx.h
+++
@@ -1,2 +0,0 @@
-extern void init_trx(uint8_t mode);
-extern void transmitte(uint8_t byte);
--- flight_controller/src/main.c
+++ flight_controller/src/main.c
@@ -2,65 +2,76 @@
 #include <stdio.h>
 #include "stm32f1xx.h"
 #include "usart1.h"
-#include "i2c1.h"
 #include "spi1.h"
-#include "radio.c"
-
-#define DEVICE 0x68
 
 int main(void) 
 {
     // Necessary for the function of all hardware
     SystemCoreClockUpdate();
 
+    // System initialization
     init_usart1();
-    // init_i2c1_master();
     init_spi1();
-    init_radio();
-    // init_trx(0U);
 
-    // RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+    usart1_write_string("STM32 Initialized\n");
 
-    // GPIOC->CRH |= GPIO_CRH_MODE13;
-    // GPIOC->CRH &= ~GPIO_CRH_CNF13;
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
 
-    // usart1_write_string("STM32 Initialized\n");
+    // Initialize PA3 for CE line
+    GPIOA->CRL |= GPIO_CRL_MODE3;                   // 0b11 output max 50MHz
+    GPIOA->CRL = ~GPIO_CRL_CNF3;                    // 0b00 output push-pull
     
+    GPIOC->CRH |= GPIO_CRH_MODE13;                   // 0b11 output max 50MHz
+    GPIOC->CRH = ~GPIO_CRH_CNF13;                    // 0b00 output push-pull
+
+    // Reset PA3 & PC13
+    GPIOA->ODR &= ~GPIO_ODR_ODR3;
+    GPIOC->ODR &= ~GPIO_ODR_ODR13;
+
+    for(int i = 0; i < 100000; i++);
+
+    uint8_t status[2];
+    uint8_t command[2];
+
+    command[0] = 0b00100000;                        // write config register
+    command[1] = 0b00000011;                        // PWR_UP | PRIM_RX
+    spi1_buffer_transaction(command, status, sizeof(command));
+
+    for(int i = 0; i < 100000; i++);
+
+    command[0] = 0b00100000 | 0x02;                 // write EN_RXADDR
+    command[1] = 0b00000001;                        // enable pipe 0 address
+    spi1_buffer_transaction(command, status, sizeof(command));
+
+    uint8_t pipe_address[6];
+    uint8_t pipe_status[6];
+    pipe_address[0] = 0b00100000 | 0x0A;            // write RX_ADDR_P0 => '00001'
+    pipe_address[1] = 0x30;                         // write 0
+    pipe_address[2] = 0x30;                         // write 0
+    pipe_address[3] = 0x30;                         // write 0
+    pipe_address[4] = 0x30;                         // write 0
+    pipe_address[5] = 0x31;                         // write 1
+    spi1_buffer_transaction(pipe_address, pipe_status, sizeof(pipe_address));
+
+    for(int i = 0; i < 100000; i++);
+
+    GPIOA->ODR |= GPIO_ODR_ODR3;
+    GPIOC->ODR |= GPIO_ODR_ODR13;
+
+    for(int i = 0; i < 100000; i++);
+
+    command[0] = 0b11111111;                        // read status register NOP
+    command[1] = 0b11111111;                        // read status register NOP
+    char output[10];
+
     while (1) {
-        // uint8_t pwr_reg[2];
-        // pwr_reg[0] = 107U;
-        // pwr_reg[1] = 0U;
-        // i2c1_write_buffer(DEVICE, pwr_reg, sizeof(pwr_reg));
-        // for(int i = 0; i < 100000; i++);
-
-        // uint8_t data[4];
-        // i2c1_write_byte(DEVICE, 63U, 0U);
-        // i2c1_read_buffer(DEVICE, data, sizeof(data));
-
-        // int16_t raw_acc_z = ((data[0] << 8U) | data[1]);
-        // char str[10];
-        // sprintf(str, "%u", raw_acc_z);
-        // usart1_write_string(str);
-        // usart1_write_string("\n");
-
-        // transmitte(107U);
-        // GPIOC->ODR ^= GPIO_ODR_ODR13;
-
-        uint8_t data[6];
-        uint8_t load[6];
-        uint8_t counter = 0;
-        data[0] = 'E';
-        data[1] = 'R';
-        data[2] = 'D';
-        data[3] = 'U';
-        data[4] = 'M';
-        data[5] = '\n';
-        spi1_buffer_transaction(data, load, sizeof(data));
-
-        while (counter <= 5) {
-            usart1_write_byte(load[counter]);
-            counter++;
+        for (uint8_t counter = 0; counter <= 2; counter++) {
+            spi1_buffer_transaction(command, status, sizeof(command));
+            sprintf(output, "%u", status[counter]);
+            usart1_write_string(output);
         }
+
         for(int i = 0; i < 100000; i++);
     }
     
