aaa7437360d061a8b45b23753399a09097d9a415
erdum <erdumadnan@gmail.com>
2023-04-21 23:39:11 +0500

    Fix config miss match

--- flight_controller/src/nrf24.c
+++ flight_controller/src/nrf24.c
@@ -1,8 +1,9 @@
 #include <stdint.h>
+#include <stdio.h>
 #include "stm32f1xx.h"
 #include "spi1.h"
 
-static void config_register(uint8_t reg, uint8_t value)
+static void write_register(uint8_t reg, uint8_t value)
 {
     cs_enable();
     spi1_send_byte(0x20 | (0x1F & reg));
@@ -10,33 +11,82 @@ static void config_register(uint8_t reg, uint8_t value)
     cs_disable();
 }
 
+static uint8_t read_register(uint8_t reg)
+{
+    cs_enable();
+    spi1_send_byte(0x1F & reg);
+    uint8_t data = spi1_send_byte(reg);
+    cs_disable();
+
+    return data;
+}
+
+static void flush_rx()
+{
+    cs_enable();
+    spi1_send_byte(0xE2);
+    cs_disable();
+}
+
+static void flush_tx()
+{
+    cs_enable();
+    spi1_send_byte(0xE2);
+    cs_disable();
+}
+
 void init_trx(void)
 {
-    config_register(0x06, 1 << 5);                                      // Set 250KBPS data rate
-    config_register(0x05, 0x2F);                                        // Set channel to 2F 2447 MHz
-    config_register(0x02, 0x3F);                                        // Enable all RX pipes
-    config_register(0x11, 32U);                                         // RX_PW_P0 = 32 bytes
-    config_register(0x12, 32U);                                         // RX_PW_P1 = 32 bytes
+    // Set CE low to put chip into the standby mode
+    GPIOC->ODR &= ~GPIO_ODR_ODR13;
 
-    uint8_t address[5];
-    address[0] = 0xE8;
-    address[1] = 0xE8;
-    address[2] = 0xF0;
-    address[3] = 0xF0;
-    address[4] = 0xE1;
+    // Set CRC & CRCO coding scheme to 2 bytes
+    write_register(0x00, (1 << 2 | 1 << 3));
 
+    // Enable auto acknowledgment for data all pipes
+    write_register(0x01, 0x3F);
+
+    // Enable all data pipes
+    write_register(0x02, 0x3F);
+
+    // Set TX/RX address width to 5 bytes, AW = 11
+    write_register(0x03, 0x03);
+
+    // Set auto retransmit delay ARD = 0110 = 1500us
+    write_register(0x04, read_register(0x04) | (1 << 6 | 1 << 5));
+
+    // Set channel to 76 = 2476MHz
+    write_register(0x05, 76U);
+
+    // Set data rate to 1Mbps & PA to 0dbm
+    write_register(0x06, (1 << 1 | 1 << 2));
+
+    // Set data pipe 0 width = 32bytes RX_PW_P0
+    write_register(0x11, 32U);
+
+    // Set pipe 0 address RX_ADDR_P0
+    const uint8_t address[5] = {0xE8E8F0F0E1};
     cs_enable();
     spi1_send_byte(0x20 | 0x0A);
     spi1_buffer_transaction(address, address, sizeof(address));
     cs_disable();
 
-    GPIOC->ODR &= ~GPIO_ODR_ODR13;
-    config_register(0x00, (1 << 1) | (1 << 2));
-    GPIOC->ODR |= GPIO_ODR_ODR13;
+    // Clear status register bits RX_DR | TX_DS | MAX_RT
+    write_register(0x07, (1 << 6) | (1 << 5) | (1 << 4));
 
-    config_register(0x07, (1 << 6) | (1 << 5) | (1 << 4));
-    
-    cs_enable();
-    spi1_send_byte(0xE2);
-    cs_disable();
+    flush_rx();
+    flush_tx();
+
+    // Power up the chip PWR_UP = 1
+    write_register(0x00, read_register(0x00) | (1 << 1));
+    for(int i = 0; i < 1000000; i++);
+
+    // Set as a receiver PRIM_RX
+    write_register(0x00, read_register(0x00) | 0x01);
+
+    // Clear status register bits RX_DR | TX_DS | MAX_RT
+    write_register(0x07, (1 << 6) | (1 << 5) | (1 << 4));
+
+    // Set CE to high
+    GPIOC->ODR |= GPIO_ODR_ODR13;
 }
