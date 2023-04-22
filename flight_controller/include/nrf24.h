4dedec5df6d5d30c07a6ef5512d1fbca39b9a991
erdum <erdumadnan@gmail.com>
2023-04-22 22:29:27 +0500

    Update new methods declared

--- flight_controller/include/nrf24.h
+++ flight_controller/include/nrf24.h
@@ -1,1 +1,4 @@
-extern void init_trx(void);
+extern bool init_trx(void);
+extern uint8_t read_register(uint8_t reg);
+extern bool switch_rx(uint8_t address[5], uint8_t sizeof_address);
+extern uint8_t *dump_memory(uint8_t registers[], uint8_t num_of_registers, uint8_t data[]);
