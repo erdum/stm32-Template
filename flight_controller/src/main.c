d39ac3ccf742d9f3bf0275c6b76561557fa9b688
erdum <erdumadnan@gmail.com>
2023-05-03 21:58:05 +0500

    Update ADC1 successfully tested

--- flight_controller/src/main.c
+++ flight_controller/src/main.c
@@ -2,25 +2,27 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include "stm32f1xx.h"
-#include "usart1.h"
 #include "clock.h"
+#include "usart1.h"
+#include "adc1.h"
 
 int main(void) 
 {
-    configure_clock();
-
+    configure_max_clock();
     init_usart1();
-    usart1_write_string("STM32 Initialized\n");
 
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+    GPIOC->CRH |= GPIO_CRH_MODE13;
 
-    GPIOC->CRH |= GPIO_CRH_MODE13;                      // 0b11 output max 50MHz
+    const uint8_t channels[2] = {0, 1};
+    init_adc1(channels, sizeof channels);
+
+    usart1_write_string("STM32 Initialized\n");
 
     while (1) {
-        char out[6];
-        sprintf(out, "0x%X\n", 0xEE);
+        char out[40];
+        sprintf(out, "ADC0: %i, ADC1: %i\n", read_adc1(0), read_adc1(1));
         usart1_write_string(out);
     }
-    
     return 0;
 }
