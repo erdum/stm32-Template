e2ddd254f353437878ecee24331b215efa1f0c4c
Erdum <erdumadnan@gmail.com>
2023-05-21 22:42:33 +0500

    Update altimeter demonstration

--- flight_controller/src/main.c
+++ flight_controller/src/main.c
@@ -5,33 +5,23 @@
 #include "clock.h"
 #include "usart1.h"
 #include "i2c1.h"
+#include "bmp180.h"
 
 int main(void) 
 {
     configure_max_clock();
     usart1_init();
     i2c1_master_init();
+    bmp180_init();
 
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
     GPIOC->CRH |= GPIO_CRH_MODE13;
 
     usart1_write_string("STM32 Initialized\n");
 
-    const uint8_t barometer_address = 0x77;
-
     while (1) {
-        uint8_t regs[2] = {0xF4, 0x2E};
-        i2c1_write_buffer(barometer_address, regs, sizeof regs);
-        systick_delay_ms(5);
-
-        uint8_t data[2];
-        i2c1_write_byte(barometer_address, 0xF6, 0U);
-        i2c1_read_buffer(barometer_address, data, sizeof data);
-
-        char out[20];
-        int16_t value = data[0] << 8U | data[1];
-        sprintf(out, "%d\n", value);
-        usart1_write_string(out);
+        double altitude = bmp180_get_altitude(1009);
+        print_float(altitude, 2);
 
         GPIOC->ODR ^= GPIO_ODR_ODR13;
     }
