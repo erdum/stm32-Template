579aa5dad02796ad78ec08f0861881fae29ccd0a
Erdum <erdumadnan@gmail.com>
2023-06-24 20:40:41 +0500

    Fix syntax error

--- flight_controller/src/imu.c
+++ flight_controller/src/imu.c
@@ -17,6 +17,27 @@ static int16_t bias_error_xg = 0;
 static int16_t bias_error_yg = 0;
 static int16_t bias_error_zg = 0;
 
+void imu_get_raw_data(int16_t *xa, int16_t *ya, int16_t *za, int16_t *xg, int16_t *yg, int16_t *zg, int16_t *temp)
+{
+    uint8_t power_reg[2] = {0x6B, 0x00};
+    i2c1_write_buffer(imu_address, power_reg, sizeof power_reg);
+
+    uint8_t DLPF_reg[2] = {0x1A, 0x04};
+    i2c1_write_buffer(imu_address, DLPF_reg, sizeof DLPF_reg);
+
+    uint8_t data[14];
+    i2c1_write_byte(imu_address, 0x3B, 0U);
+    i2c1_read_buffer(imu_address, data, sizeof data);
+
+    *xa = (data[0] << 8U) | data[1];
+    *ya = (data[2] << 8U) | data[3];
+    *za = (data[4] << 8U) | data[5];
+    *temp = ((int16_t)((data[6] << 8U) | data[7]) / 340) + 36.53;
+    *xg = (data[8] << 8U) | data[9];
+    *yg = (data[10] << 8U) | data[11];
+    *zg = (data[12] << 8U) | data[13];
+}
+
 void imu_calibirate(void)
 {
     int16_t xa, ya, za, xg, yg, zg, imu_temp;
@@ -41,31 +62,10 @@ void imu_calibirate(void)
 
 }
 
-void imu_get_raw_data(int16_t *xa, int16_t *ya, int16_t *za, int16_t *xg, int16_t *yg, int16_t *zg, int16_t *temp)
-{
-	uint8_t power_reg[2] = {0x6B, 0x00};
-    i2c1_write_buffer(imu_address, power_reg, sizeof power_reg);
-
-    uint8_t DLPF_reg[2] = {0x1A, 0x04};
-    i2c1_write_buffer(imu_address, DLPF_reg, sizeof DLPF_reg);
-
-    uint8_t data[14];
-    i2c1_write_byte(imu_address, 0x3B, 0U);
-    i2c1_read_buffer(imu_address, data, sizeof data);
-
-    *xa = (data[0] << 8U) | data[1];
-    *ya = (data[2] << 8U) | data[3];
-    *za = (data[4] << 8U) | data[5];
-    *temp = ((int16_t)((data[6] << 8U) | data[7]) / 340) + 36.53;
-    *xg = (data[8] << 8U) | data[9];
-    *yg = (data[10] << 8U) | data[11];
-    *zg = (data[12] << 8U) | data[13];
-}
-
 void imu_get_values(int16_t *xa, int16_t *ya, int16_t *za, int16_t *xg, int16_t *yg, int16_t *zg)
 {
     int16_t XA, YA, ZA, XG, YG, ZG, imu_temp;
-    imu_get_raw_data(&XA, &YA, &ZA, &XG, &YG,ZGzg, &imu_temp);
+    imu_get_raw_data(&XA, &YA, &ZA, &XG, &YG, &ZG, &imu_temp);
 
     *xa = XA - bias_error_xa;
     *ya = YA - bias_error_ya;
