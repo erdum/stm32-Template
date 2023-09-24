2a691e203626eea66454465dbf38497fc3cba66b
Erdum <erdumadnan@gmail.com>
2023-09-24 14:56:03 +0500

    Fix CS pin not configuring due to type, baud rate increased, simplified data transfer, CSN hold time reduced

--- flight_controller/src/spi1.c
+++ flight_controller/src/spi1.c
@@ -7,18 +7,18 @@ void spi1_master_init(void)
     RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
     RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
 
-    GPIOA->CRL &= ~(GPIO_CRL_MODE5 | GPIO_CRL_CNF5);         // reset PA4  MODE 0b00
+    GPIOA->CRL &= ~(GPIO_CRL_MODE4 | GPIO_CRL_CNF4);         // reset PA4  MODE 0b00
     GPIOA->CRL &= ~(GPIO_CRL_MODE5 | GPIO_CRL_CNF5);         // reset PA5  MODE 0b00
     GPIOA->CRL &= ~(GPIO_CRL_MODE6 | GPIO_CRL_CNF6);         // reset PA6  MODE 0b00
     GPIOA->CRL &= ~(GPIO_CRL_MODE7 | GPIO_CRL_CNF7);         // reset PA7  MODE 0b00
 
-    GPIOA->CRL |= GPIO_CRL_MODE4_0;                         // SS 0b01 10MHz output push-pull
+    GPIOA->CRL |= GPIO_CRL_MODE4;                           // SS 0b11 50MHz output push-pull
     GPIOA->CRL |= GPIO_CRL_MODE5_0 | GPIO_CRL_CNF5_1;       // SCK 0b01 10MHz output alt push-pull
     GPIOA->CRL |= GPIO_CRL_CNF6_0;                          // MISO 0b01 Input floating
     GPIOA->CRL |= GPIO_CRL_MODE7_0 | GPIO_CRL_CNF7_1;       // MOSI 0b01 10MHz output alt push-pull
 
     SPI1->CR1 |= SPI_CR1_MSTR;                              // master configuration
-    SPI1->CR1 |= SPI_CR1_BR_2;                              // baud rate clk/32
+    SPI1->CR1 |= SPI_CR1_BR_1;                              // baud rate clk / 8 = 72MHz / 8 = 9MHz
     SPI1->CR1 &= ~(SPI_CR1_RXONLY);                         // full duplex
     SPI1->CR1 &= ~(SPI_CR1_LSBFIRST);                       // MSB first
     SPI1->CR1 &= ~(SPI_CR1_CPOL);                           // clock 0 when idle
@@ -27,7 +27,7 @@ void spi1_master_init(void)
     SPI1->CR1 &= ~(SPI_CR1_BIDIMODE);                       // 2-line unidirectional data transfer mode
     SPI1->CR1 |= SPI_CR1_SSM;                               // software slave management enable
     SPI1->CR1 |= SPI_CR1_SSI;                               // enable internal slave select
-    SPI1->CR1 |= SPI_CR1_SPE;                               // enable serial peripheral
+    SPI1->CR1 |= SPI_CR1_SPE;
 }
 
 void spi1_buffer_transaction(
@@ -35,16 +35,10 @@ void spi1_buffer_transaction(
     uint8_t *rx_buffer,
     uint8_t sizeof_buffer
 ) {
-    volatile int tmp;
     uint8_t counter = (sizeof_buffer / sizeof(uint8_t));
 
     while (counter > 0U) {
-
-        SPI1->DR = *tx_buffer;
-        while (!(SPI1->SR & SPI_SR_TXE));
-
-        while (!(SPI1->SR & SPI_SR_RXNE));
-        *rx_buffer = SPI1->DR;
+        *rx_buffer = spi1_send_byte(*tx_buffer);
 
         tx_buffer++;
         rx_buffer++;
@@ -52,26 +46,18 @@ void spi1_buffer_transaction(
     }
 
     while (SPI1->SR & SPI_SR_BSY);
-
-    tmp = SPI1->DR;
-    tmp = SPI1->SR;
 }
 
 uint8_t spi1_send_byte(uint8_t data)
 {
-    volatile int tmp;
     uint8_t buffer;
     
     SPI1->DR = data;
-    while(!(SPI1->SR & SPI_SR_TXE));
     
     while (!(SPI1->SR & SPI_SR_RXNE));
     buffer = SPI1->DR;
 
-    while (SPI1->SR & SPI_SR_BSY);
-
-    tmp = SPI1->DR;
-    tmp = SPI1->SR;
+    while(!(SPI1->SR & SPI_SR_TXE));
 
     return buffer;
 }
@@ -87,5 +73,5 @@ void cs_disable(void)
     // CS Disable
     GPIOA->ODR |= GPIO_ODR_ODR4;
 
-    for(int i = 0; i < 80000; i++);
+    for(int i = 0; i < 10; i++);
 }
