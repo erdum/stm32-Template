9cf6620302ab12b349006a13ba6c97fc58dff157
Erdum <erdumadnan@gmail.com>
2023-09-25 09:48:27 +0500

    Update basic 3 channel receiver

--- flight_controller/src/main.c
+++ flight_controller/src/main.c
@@ -1,87 +1,55 @@
 #include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include "stm32f1xx.h"
 #include "clock.h"
-#include "timer1_pwm.h"
+#include "systick.h"
+#include "usart1.h"
+#include "nrf24.h"
 #include "timer2_pwm.h"
 #include "timer3_pwm.h"
-#include "timer4_pwm.h"
-#include "systick.h"
 
 // Output config
 #define output_pwm_period 20
 #define throttle_channel 3              // PA2      timer2
 #define elevator_channel 4              // PA3      timer2
-#define rudder_channel 1                // PA8      timer1
-#define ailerons_channel 3              // PB0      timer3
-#define flaps_channel 4                 // PB1      timer3
-#define extra_channel_1 3               // PB8      timer4
-#define extra_channel_2 4               // PB9      timer4
-
-#define POST_delay 500
+#define rudder_channel 3                // PB0      timer3
 
 int main(void) 
 {
     // Configure device max clock speed 72MHz
     configure_max_clock();
+    
+    usart1_init();
 
-    // Turn board led on indicating Power-On-Self-Test
-    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
-    GPIOC->CRH |= GPIO_CRH_MODE13;
-
-    // Configure timer1 to generate pwm of 20ms on pins PA8 to PA11
-    timer1_pwm_init(output_pwm_period);
-
-    // Configure timer2 to generate pwm of 20ms on pins PA0 to PA4
     timer2_pwm_init(output_pwm_period);
-
-    // Configure timer3 to generate pwm of 20ms on pins PA6, PA7, PB0, PB1
     timer3_pwm_init(output_pwm_period);
 
-    // Configure timer4 to generate pwm of 20ms on pins PB6 to PB9
-    timer4_pwm_init(output_pwm_period);
-
-    // Enable pwm on all the required pins
-    timer1_pwm_enable(rudder_channel);
     timer2_pwm_enable(throttle_channel);
     timer2_pwm_enable(elevator_channel);
-    timer3_pwm_enable(ailerons_channel);
-    timer3_pwm_enable(flaps_channel);
-    timer4_pwm_enable(extra_channel_1);
-    timer4_pwm_enable(extra_channel_2);
+    timer3_pwm_enable(rudder_channel);
 
-    // PWM channels Power-On-Self-Test ----------------------------------------
-    for (uint8_t counter = 0; counter < 10; counter++) {
+    timer2_pwm_write(throttle_channel, 2);
+    timer2_pwm_write(elevator_channel, 1.5);
+    timer3_pwm_write(rudder_channel, 1.5);
 
-        if (counter < 4) {
-            timer2_pwm_write(throttle_channel, 2);
-        } else {
-            timer2_pwm_write(throttle_channel, 1);
-        }
+    systick_delay_ms(1000);
+    timer2_pwm_write(throttle_channel, 1);
 
-        if (counter % 2) {
-            timer1_pwm_write(rudder_channel, 1.2);
-            timer2_pwm_write(elevator_channel, 1.2);
-            timer3_pwm_write(ailerons_channel, 1.2);
-            timer3_pwm_write(flaps_channel, 1.2);
-            timer4_pwm_write(extra_channel_1, 1.2);
-            timer4_pwm_write(extra_channel_2, 1.2);
-        } else {
-            timer1_pwm_write(rudder_channel, 1.8);
-            timer2_pwm_write(elevator_channel, 1.8);
-            timer3_pwm_write(ailerons_channel, 1.8);
-            timer3_pwm_write(flaps_channel, 1.8);
-            timer4_pwm_write(extra_channel_1, 1.8);
-            timer4_pwm_write(extra_channel_2, 1.8);
-        }
+    uint8_t radio_address[5] = { 0xE7, 0xE7, 0xE7, 0xE7, 0xE7 };
 
-        systick_delay_ms(POST_delay);
-    }
-    // -------------------------------------------------------------------------
+    trx_init(radio_address, sizeof(radio_address), 108U);
+    trx_switch_rx();
 
     while (1) {
-        // Toggle board led indicating Power-On-Self-Test complete
-        GPIOC->ODR ^= GPIO_ODR_ODR13;
-        systick_delay_ms(500);
+
+        if (trx_data_available()) {
+            uint8_t payload[32];
+            trx_receive(payload, sizeof(payload));
+            timer2_pwm_write(throttle_channel, 1 + (payload[0] / 100.0));
+            timer2_pwm_write(elevator_channel, 1 + (payload[1] / 100.0));
+            timer3_pwm_write(rudder_channel, 1 + (payload[2] / 100.0));
+        }
     }
     return 0;
 }
